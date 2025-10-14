eae3c2e6ccb7e6262b5a98054f60c097c6adb0e9
Polina Tomina <polina@HOME-PC>
2025-10-15 02:15:30 +0300

    Implemented WarehouseService

--- Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
+++ Lab6TestTask/Lab6TestTask/Services/Implementations/WarehouseService.cs
@@ -1,6 +1,7 @@
 ﻿using Lab6TestTask.Data;
 using Lab6TestTask.Models;
 using Lab6TestTask.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lab6TestTask.Services.Implementations;
 
@@ -19,11 +20,33 @@ public class WarehouseService : IWarehouseService
 
     public async Task<Warehouse> GetWarehouseAsync()
     {
-        throw new NotImplementedException();
+        var warehouse = await _dbContext.Warehouses
+            .Select(w => new
+            {
+                Warehouse = w,
+                TotalValue = w.Products
+                .Where(p => p.Status == Enums.ProductStatus.ReadyForDistribution)
+                .Sum(p => p.Price * p.Quantity)
+            })
+            .OrderByDescending(x => x.TotalValue)
+            .Select(x => x.Warehouse)
+            .FirstOrDefaultAsync();
+
+        if (warehouse == null)
+            throw new InvalidOperationException("Warehouse not found");
+
+        return warehouse;
     }
 
     public async Task<IEnumerable<Warehouse>> GetWarehousesAsync()
     {
-       throw new NotImplementedException();
+        var warehouses = await _dbContext.Warehouses
+            .Where(w => w.Products.Any(p =>
+            p.ReceivedDate.Year == 2025 && 
+            p.ReceivedDate.Month >= 4 && 
+            p.ReceivedDate.Month <= 6))
+            .ToListAsync();
+
+        return warehouses;
     }
 }
